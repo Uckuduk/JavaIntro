34e4f02333b1a142d52f10d6edd7325f13e6690d
Andrey Kisel <82019240+Uckuduk@users.noreply.github.com>
2021-07-04 15:10:50 +0300

    Ready branching programs

--- FirstModule/src/by/jonline/module1/main/Main.java
+++ FirstModule/src/by/jonline/module1/main/Main.java
@@ -4,12 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(LinearProgram.firstTask());
-        System.out.println(LinearProgram.secondTask());
-        System.out.println(LinearProgram.thirdTask());
-        System.out.println(LinearProgram.fourthTask());
-        System.out.println(LinearProgram.fifthTask());
-        System.out.println(LinearProgram.sixthTask());
+        System.out.println("1.1) " + LinearProgram.firstTask());
+        System.out.println("1.2) " + LinearProgram.secondTask());
+        System.out.println("1.3) " + LinearProgram.thirdTask());
+        System.out.println("1.4) " + LinearProgram.fourthTask());
+        System.out.println("1.5) " + LinearProgram.fifthTask());
+        System.out.println("1.6) " + LinearProgram.sixthTask());
 
+        System.out.println("2.1) ");
+        BranchingProgram.isTriangle();
+
+        System.out.println("2.2) " + BranchingProgram.secondTask());
+        System.out.println("2.3) " + BranchingProgram.thirdTask());
+        System.out.println("2.4) " + BranchingProgram.fourthTask());
+        System.out.println("2.5) " + BranchingProgram.fifthTask());
     }
 }
