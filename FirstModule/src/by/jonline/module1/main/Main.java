0b4586184149489dbd4025fc0bf3c4583f421788
Andrey Kisel <82019240+Uckuduk@users.noreply.github.com>
2021-07-04 14:19:59 +0300

    Ready linear programs

--- FirstModule/src/by/jonline/module1/main/Main.java
+++ FirstModule/src/by/jonline/module1/main/Main.java
@@ -3,8 +3,13 @@ package by.jonline.module1.main;
 public class Main {
 
     public static void main(String[] args) {
-        BranchingProgram.isTriangle();
 
+        System.out.println(LinearProgram.firstTask());
+        System.out.println(LinearProgram.secondTask());
+        System.out.println(LinearProgram.thirdTask());
+        System.out.println(LinearProgram.fourthTask());
         System.out.println(LinearProgram.fifthTask());
+        System.out.println(LinearProgram.sixthTask());
+
     }
 }
